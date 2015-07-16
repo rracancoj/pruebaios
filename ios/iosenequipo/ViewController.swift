07549c923ad86ced99dcb1f7a45a03efa9ede29b
CICE <CICE@08USERMAC07POV.local>
2015-07-16 09:49:25 +0200

    prueba1111

--- ios/iosenequipo/ViewController.swift
+++ ios/iosenequipo/ViewController.swift
@@ -1,18 +1,15 @@
 //
 //  ViewController.swift
-//  prueba3
+//  iosenequipo
 //
-//  Created by CICE on 16/7/15.
-//  Copyright (c) 2015 Doneone. All rights reserved.
+//  Created by formador on 16/7/15.
+//  Copyright (c) 2015 cice. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    //PRUEBA
-    
-    //PROBANDO GIT2 PRUEBA
 
     override func viewDidLoad() {
         super.viewDidLoad()

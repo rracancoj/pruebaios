842ada53ff904bcebba3b5572c86f1eb3728a8e2
CICE <CICE@08USERMAC07POV.local>
2015-07-16 09:21:45 +0200

    Initial Commit

---
+++ prueba3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prueba3
+//
+//  Created by CICE on 16/7/15.
+//  Copyright (c) 2015 Doneone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
